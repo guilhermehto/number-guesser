24bf9bcf9a806a1c5db225ebe2e22cfc87f993b4
Guilherme Oliveira <htoguilherme@gmail.com>
2018-03-02 07:11:46 -0300

    Guessing player number

--- Game.gd
+++ Game.gd
@@ -5,18 +5,28 @@ var min_guessed = 0
 var max_guessed = 1000
 
 func _ready():
+	guess = (min_guessed + max_guessed) / 2
 	print("----------------------")
 	print("----------------------")
 	print("----------------------")
 	print("Hello from Number Guesser!")
 	print("I'll guess any number that you think between 0 and 1000")
+	print("Is " + str(guess) + " your number?")
 	
 
 func _process(delta):
 	if Input.is_action_just_pressed("up"):
-		print("Up pressed")
+		_try_guess("up")
 	elif Input.is_action_just_pressed("down"):
-		print("Down pressed")
+		_try_guess("down")
 
+#type up = greater or down = lesser
+func _try_guess(type):
+	if type == "up":
+		min_guessed = guess
+	else:
+		max_guessed = guess
+	guess = (min_guessed + max_guessed) / 2
+	print("Is " + str(guess) + " your number?")
 
 
